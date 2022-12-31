b4b8a2a30cc531dda692708cdffbe09512484b39
hzk2021 <hezhenkai1999@gmail.com>
2022-12-31 18:20:27 +0800

    Initial commit

---
+++ test/it3122_cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const It3122Cdk = require('../lib/it3122_cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/it3122_cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new It3122Cdk.It3122CdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
